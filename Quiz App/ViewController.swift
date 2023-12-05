2573a1e8bc699101d92dfa1f5e080a67252ea376
Jose Trujillo <joseh.trujillo11@gmail.com>
2023-12-05 14:19:07 -0500

    Done

--- Quiz App/ViewController.swift
+++ Quiz App/ViewController.swift
@@ -9,7 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 var index = 0
+var score = 0
+    
     var Questions:[Quiz] = [Quiz(question: "How much does 800 robux cost?", answer: "10"), Quiz(question: "How much does 10,000 robux cost?", answer: "100"), Quiz(question: "What is the most played game in Roblox?", answer: "Brookhaven"),Quiz(question: "What is the most expensive catalog item in Roblox?", answer: "Dominus Empyreus"), Quiz(question: "What game revloves around buying and trading pets?", answer: "Adopt Me"), Quiz(question: "What year was Roblox made?", answer: "2006"), Quiz(question: "What famous horror game was inspired by Peppa Pig?", answer: "Piggy")]
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,27 +22,23 @@ var index = 0
         
         
                                      
-        Label1.text = Questions[0].question
+        questionName.text = Questions[0].question
         
     }
 
-    @IBOutlet weak var Label1: UILabel!
+    @IBOutlet weak var questionName: UILabel!
     
-    @IBAction func Button(_ sender: Any) {
+    @IBOutlet weak var answerTextField: UITextField!
+    
+    @IBAction func replay(_ sender: Any) {
+        
     }
     
-    @IBOutlet weak var Label2: UILabel!
-    
-    
-    
+    @IBAction func Checkanswer(_ sender: Any) {
+      
+    }
     @IBAction func next(_ sender: UIButton) {
-        if index < Questions.count
-        {
-            index += 1
-            Label1.text = Questions[index].question
-
-        }
     }
-                                 
+    
 }
 
